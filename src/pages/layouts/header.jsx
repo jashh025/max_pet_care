780381e39ef4dfba12cf223cdec9aa7db2be6030
jashh025 <jashh025@gmail.com>
2026-04-19 02:47:36 -0700

    Update header.jsx

--- src/pages/layouts/header.jsx
+++ src/pages/layouts/header.jsx
@@ -17,28 +17,36 @@ const Header = () => {
   return (
     <header className={`header ${scrolled ? 'scrolled' : ''}`}>
       <div className="header-container">
-        <a href="#" className="logo-container">
+
+        {/* LOGO */}
+        <Link to="/" className="logo-container">
           <img
             src="https://i.ibb.co/8DYyJ8fp/Max-logo-1-1.png"
             alt="MaxPetCare logo"
             className="logo-img"
           />
           <span className="logo-text">Max Pet Care</span>
-        </a>
+        </Link>
 
+        {/* NAV */}
         <nav className={`nav ${menuOpen ? 'open' : ''}`}>
-          <a href="/home" onClick={() => setMenuOpen(false)}>
+
+          <Link to="/" onClick={() => setMenuOpen(false)}>
             Home
-          </a>
-          <a href="/services" onClick={() => setMenuOpen(false)}>
+          </Link>
+
+          <Link to="/services" onClick={() => setMenuOpen(false)}>
             Services
-          </a>
-          <a href="/gallery" onClick={() => setMenuOpen(false)}>
+          </Link>
+
+          <Link to="/gallery" onClick={() => setMenuOpen(false)}>
             Gallery
-          </a>
-          <a href="/aboutus" onClick={() => setMenuOpen(false)}>
+          </Link>
+
+          <Link to="/aboutus" onClick={() => setMenuOpen(false)}>
             About Us
-          </a>
+          </Link>
+
           <Link to="/contact" onClick={() => setMenuOpen(false)}>
             Contact
           </Link>
@@ -47,6 +55,7 @@ const Header = () => {
             <a href="tel:+919952731063" className="call-btn">
               <i className="fas fa-phone-alt"></i> Call
             </a>
+
             <a
               href="https://wa.me/9952731063"
               target="_blank"
@@ -56,8 +65,10 @@ const Header = () => {
               <i className="fab fa-whatsapp"></i> WhatsApp
             </a>
           </div>
+
         </nav>
 
+        {/* HAMBURGER */}
         <div
           className={`hamburger ${menuOpen ? 'open' : ''}`}
           onClick={() => setMenuOpen(!menuOpen)}
@@ -66,6 +77,7 @@ const Header = () => {
           <span></span>
           <span></span>
         </div>
+
       </div>
     </header>
   );
