9428eccdcb3660d5f1cb78e377c7f0a499b39ff0
jashh025 <jashh025@gmail.com>
2026-04-19 08:22:34 -0700

    Update gallery.jsx

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -17,7 +17,7 @@ import dog11 from '../uploads/DSC05468.JPG';
 import dog12 from '../uploads/IMG_20160811_092944.jpg';
 import dog13 from '../uploads/IMG_20180422_105812.JPG';
 import dog14 from '../uploads/IMG_20190307_100421.jpg';
-import dogVideo from '../uploads/your-video.mp4';
+import dogVideo from '../uploads/VIDEO-2025-05-03-18-01-59.mov';
 
 export default function Gallery() {
   const images = [dog1, dog2, dog3, dog4, dog5, dog6, dog7, dog8, dog9, dog10, dog11, dog12, dog13, dog14];
