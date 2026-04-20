74647c05461004068c4b15a717944c59121cf2eb
jashh025 <jashh025@gmail.com>
2026-04-20 08:09:33 -0700

    Update contact.jsx

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -74,27 +74,23 @@ const ContactUs = () => {
             </div>
           </div>
 
-          {/* RIGHT SIDE - FORM + MAP */}
           <div className="contact-right">
-
-            {/* FORM */}
             <form ref={formRef} onSubmit={sendEmail} className="contact-form">
               <input type="text" name="name" placeholder="Your Name" required />
               <input type="email" name="email" placeholder="Your Email" required />
               <textarea name="message" placeholder="Your Message" required />
               <button type="submit">Send Message</button>
             </form>
+          </div>
 
-            {/* MAP */}
-            <div className="map-box">
+          <div className="map-box">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3929.4713885372025!2d78.16994707393452!3d9.977864273417465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b00c6939dcffaf5%3A0x4bdd7eb8ed940a90!2sMAX%20PET%20CARE%20MADURAI-%20Dog%20hostel%20in%20madurai!5e0!3m2!1sen!2sin!4v1775141137609!5m2!1sen!2sin"
                 allowFullScreen=""
                 loading="lazy"
               ></iframe>
-            </div>
-
           </div>
+          
         </div>
       </div>
 
